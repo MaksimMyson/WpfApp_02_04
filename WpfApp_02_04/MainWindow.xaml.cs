b3ed96e4b8ffb56d74fd9c5533c00d1a274175f8
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-04-14 14:04:42 +0300

    ex4

--- WpfApp_02_04/MainWindow.xaml.cs
+++ WpfApp_02_04/MainWindow.xaml.cs
@@ -1,24 +1,45 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_02_04
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateAndSaveNumbers_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Random random = new Random();
+            int[] numbers = Enumerable.Range(0, 10000).Select(i => random.Next(-10000, 10001)).ToArray();
+
+            
+            var evenNumbers = numbers.Where(num => num % 2 == 0).ToArray();
+            var oddNumbers = numbers.Where(num => num % 2 != 0).ToArray();
+
+            
+            string evenFilePath = "even_numbers.txt";
+            string oddFilePath = "odd_numbers.txt";
+            File.WriteAllLines(evenFilePath, evenNumbers.Select(num => num.ToString()));
+            File.WriteAllLines(oddFilePath, oddNumbers.Select(num => num.ToString()));
+
+           
+            int totalNumbers = numbers.Length;
+            int evenCount = evenNumbers.Length;
+            int oddCount = oddNumbers.Length;
+            double evenPercentage = (double)evenCount / totalNumbers * 100;
+            double oddPercentage = (double)oddCount / totalNumbers * 100;
+
+            string statisticsText = $"Загальна кількість чисел: {totalNumbers}\n" +
+                                    $"Парних чисел: {evenCount} ({evenPercentage:F2}%)\n" +
+                                    $"Непарних чисел: {oddCount} ({oddPercentage:F2}%)";
+
+            StatisticsTextBox.Text = statisticsText;
         }
     }
 }
