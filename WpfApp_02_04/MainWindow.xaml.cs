aeee36c986fe50810a48f2fcd77bcc8b52d611e4
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-04-14 14:25:14 +0300

    ex2

--- WpfApp_02_04/MainWindow.xaml.cs
+++ WpfApp_02_04/MainWindow.xaml.cs
@@ -1,24 +1,48 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_02_04
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ReplaceWords_Click(object sender, RoutedEventArgs e)
+        {
+            string searchWord = SearchTextBox.Text.Trim();
+            string replaceWord = ReplaceTextBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(searchWord) || string.IsNullOrEmpty(replaceWord))
+            {
+                MessageBox.Show("Будь ласка, введіть слово для пошуку та слово для заміни.");
+                return;
+            }
+
+            string filePath = "C:\\Users\\user\\source\\repos\\WpfApp_02_04\\wpf_02_04.txt"; 
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Файл не знайдено.");
+                return;
+            }
+
+            string fileContent = File.ReadAllText(filePath);
+
+            int replacementsCount = ReplaceWords(ref fileContent, searchWord, replaceWord);
+
+            File.WriteAllText(filePath, fileContent);
+
+            ResultTextBox.Text = $"Кількість замін: {replacementsCount}";
+        }
+
+        private int ReplaceWords(ref string text, string searchWord, string replaceWord)
+        {
+            int count = Regex.Matches(text, searchWord).Count;
+            text = text.Replace(searchWord, replaceWord);
+            return count;
         }
     }
 }
