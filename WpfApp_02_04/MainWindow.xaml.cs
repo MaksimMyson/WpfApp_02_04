9132e16d056718892b83e4e3cc8c17d48c48a8a4
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-04-14 14:12:14 +0300

    ex6

--- WpfApp_02_04/MainWindow.xaml.cs
+++ WpfApp_02_04/MainWindow.xaml.cs
@@ -1,24 +1,45 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_02_04
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DisplayFileInfo_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = FilePathTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Будь ласка, введіть шлях до файлу.");
+                return;
+            }
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Файл не знайдено.");
+                return;
+            }
+
+            string fileContent = File.ReadAllText(filePath);
+            int sentenceCount = Regex.Matches(fileContent, @"[.!?]").Count;
+            int uppercaseCount = Regex.Matches(fileContent, @"[A-Z]").Count;
+            int lowercaseCount = Regex.Matches(fileContent, @"[a-z]").Count;
+            int vowelCount = Regex.Matches(fileContent, @"[aeiouAEIOU]").Count;
+            int consonantCount = Regex.Matches(fileContent, @"[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]").Count;
+            int digitCount = Regex.Matches(fileContent, @"[0-9]").Count;
+
+            ResultTextBox.Text = $"Кількість речень: {sentenceCount}\n" +
+                                 $"Кількість великих літер: {uppercaseCount}\n" +
+                                 $"Кількість маленьких літер: {lowercaseCount}\n" +
+                                 $"Кількість голосних літер: {vowelCount}\n" +
+                                 $"Кількість приголосних літер: {consonantCount}\n" +
+                                 $"Кількість цифр: {digitCount}";
         }
     }
 }
